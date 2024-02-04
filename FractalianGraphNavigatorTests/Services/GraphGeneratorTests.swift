1e101d369707c5a905ca2a9799e1eb3c629f445c
Przemysław Kuzia <przemek.kuzia@gmail.com>
2024-02-04 22:05:45 +0100

    Fix test (#2)

--- FractalianGraphNavigatorTests/Services/GraphGeneratorTests.swift
+++ FractalianGraphNavigatorTests/Services/GraphGeneratorTests.swift
@@ -14,28 +14,34 @@ class GraphGeneratorTests: XCTestCase {
     func testGenerateRandomGraph() async {
         let generator = RandomGraphGenerator()
 
-        let graph = await generator.generateGraph(nodes: 5, maxEdgesPerNode: 3)
+        do {
+            let graph = try await generator.generateGraph(nodes: 5, maxEdgesPerNode: 3)
 
-        XCTAssertEqual(graph.nodes.count, 5)
-        XCTAssertTrue(graph.nodes.keys.allSatisfy { $0.hasPrefix("n") })
+            XCTAssertEqual(graph.nodes.count, 5)
+            XCTAssertTrue(graph.nodes.keys.allSatisfy { $0.hasPrefix("n") })
 
-        let totalEdges = graph.nodes.values.reduce(0) { $0 + $1.neighbors.count }
-        XCTAssertLessThanOrEqual(totalEdges, 3 * 5)
+            let totalEdges = graph.nodes.values.reduce(0) { $0 + $1.neighbors.count }
+            XCTAssertLessThanOrEqual(totalEdges, 3 * 5)
+        } catch {
+            XCTFail("Unexpected error: \(error)")
+        }
     }
 
     func testGenerateAcyclicGraph() async {
         let generator = AcyclicGraphGenerator()
 
-        let graph = await generator.generateDAG(width: 3, height: 3, probability: 50)
+        do {
+            let graph = try await generator.generateDAG(width: 3, height: 3, probability: 50)
 
-        XCTAssertEqual(graph.nodes.count, 9)
-        XCTAssertTrue(graph.nodes.keys.allSatisfy { $0.hasPrefix("n") })
+            XCTAssertEqual(graph.nodes.count, 9)
+            XCTAssertTrue(graph.nodes.keys.allSatisfy { $0.hasPrefix("n") })
 
-        let totalEdges = graph.nodes.values.reduce(0) { $0 + $1.neighbors.count }
-
-        for (_, startNode) in graph.nodes {
-            var visitedNodes: Set<String> = []
-            XCTAssertFalse(hasCycle(from: startNode, visitedNodes: &visitedNodes))
+            for (_, startNode) in graph.nodes {
+                var visitedNodes: Set<String> = []
+                XCTAssertFalse(hasCycle(from: startNode, visitedNodes: &visitedNodes))
+            }
+        } catch {
+            XCTFail("Unexpected error: \(error)")
         }
     }
 
